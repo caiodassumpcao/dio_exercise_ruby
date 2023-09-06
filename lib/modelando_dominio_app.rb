51420424bf68bdc16dd4ae98a0bba210fb02025c
caiodassumpcao <caio.rodri.d@gmail.com>
2023-09-06 12:21:31 -0300

    Fix modelando_dominio_app.rb, modelando_dominio_produto.rb, modelando_dominio_loja.rb: Modeling a store domain

--- lib/modelando_dominio_app.rb
+++ lib/modelando_dominio_app.rb
@@ -1,4 +1,17 @@
-require 'produto'
-require 'loja'
+require_relative 'modelando_dominio_produto'
+require_relative 'modelando_dominio_loja'
 
-produto = Produto.new
+
+produto1 = Produto.new
+produto1.nome = 'Mouse'
+produto1.preco = 89.99
+produto2 = Produto.new
+produto2.nome = 'Teclado'
+produto2.preco = 120.00
+produto3 = Produto.new
+produto3.nome = 'Headfone'
+produto3.preco = 147.50
+
+Loja.new(produto1.nome, produto1.preco).comprar
+Loja.new(produto2.nome, produto2.preco).comprar
+Loja.new(produto3.nome, produto3.preco).comprar
