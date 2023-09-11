99ad44797895d23676f2eb18bbe584626beac82e
caiodassumpcao <caio.rodri.d@gmail.com>
2023-09-11 12:52:22 -0300

    Fix desafio_soma_indice.rb: Sum of objects at even indexes

--- lib/desafio_soma_indice.rb
+++ lib/desafio_soma_indice.rb
@@ -1,22 +1,13 @@
-#Desafio
-# Crie um programa que, dadas duas listas de números inteiros e tamanho igual, some os índices pares de cada uma delas e print no console uma nova lista resultante dessa operação.
-array_1 = [1, 2, 3, 4, 5]
-array_2 = [1, 2, 3, 4, 5]
 
-
-
-def soma(array_1, array_2)
-  tamanho = array_1.length
-  array_final = []
-
-  tamanho.times do |n|
-    if n.even?
-      numero_index = array_1[n] + array_2[n]
-      array_final.push(numero_index)
-    end
+array1 = [1, 2, 3, 4, 5, 6]
+array2 = [1, 2, 3, 4, 5, 6]
+resultado = []
+for i in 0...array1.length
+  if i % 2 == 0
+    resultado << array1[i] + array2[i]
   end
-  array_final
 end
 
-resultado = soma(array_1, array_2)
-puts resultado
+
+
+puts resultado.join(',')
