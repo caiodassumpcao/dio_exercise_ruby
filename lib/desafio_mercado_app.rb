8a3a278a591a366d1300426b435e0b739a1d085f
caiodassumpcao <caio.rodri.d@gmail.com>
2023-09-12 10:54:51 -0300

    Feat desafio_mercado(app, loja, produto).rb: Printing items sold at a market

--- lib/desafio_mercado_app.rb
+++ lib/desafio_mercado_app.rb
@@ -10,9 +10,14 @@ produto2.preco = 120.00
 produto3 = Produto.new
 produto3.nome = 'Headfone'
 produto3.preco = 147.50
-custo_total = produto1.preco + produto2.preco + produto3.preco
+produto4 = Produto.new
+produto4.nome = 'Placa de video'
+produto4.preco = 1080.00
+custo_total = produto1.preco + produto2.preco + produto3.preco + produto4.preco
 
 Mercado.new(produto1.nome, produto1.preco).comprar
 Mercado.new(produto2.nome, produto2.preco).comprar
 Mercado.new(produto3.nome, produto3.preco).comprar
+Mercado.new(produto4.nome, produto4.preco).comprar
 puts "Custo total da compra foi de R$: #{custo_total}"
+
---
+++ lib/mvc_conceitos.rb
@@ -0,0 +1,9 @@
+puts "MODEL:"
+conceitos_model = ["Armazena dados", "Responde ao controller", "Localizaçao das regras e validaçoes"]
+puts "Conceitos:\n #{conceitos_model[0]}\n #{conceitos_model[1]}\n #{conceitos_model[2]}"
+
+puts "CONTROLLER:\n"
+
+conceitos_contr = ["Envia dados ao modelo", "Retorna a resposta ao View", "Considerado como garçom"]
+puts "Conceitos:\n #{conceitos_contr[0]}\n #{conceitos_contr[1]}\n #{conceitos_contr[2]}"
+
