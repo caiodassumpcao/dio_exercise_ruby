caa03c03a897b2b5cfe0841426f103e2eb3074f5
caiodassumpcao <caio.rodri.d@gmail.com>
2023-09-20 18:43:50 -0300

    Fix lambda.rb: Training lambda structure

--- lib/lambda.rb
+++ lib/lambda.rb
@@ -21,4 +21,27 @@ t_lambda = -> (names){ names.each { |name |puts name}}
 t_lambda.call(names)
 
 
-puts "Lambda com parametros "
+puts "Lambda de varias linhas "
+
+meu_lambda = lambda do |numbers|
+  i = 0
+  puts "Numero atual + Proximo numero"
+  numbers.each do |number|
+    return if numbers[i] == numbers.last
+    puts "(#{numbers[i]}) + (#{numbers[i + 1]})"
+    puts numbers[i] + numbers[i + 1]
+    i += 1
+  end
+end
+
+numbers = [1, 2, 3, 4]
+
+meu_lambda.call(numbers)
+
+def teste(first_lambda, second_lambda)
+  first_lambda.call
+  second_lambda.call
+end
+
+first_lambda = lambda { puts "my first lambda"}
+
