d5d0f0b798de7c808f42cd181b1cc3e3c8cd5fd6
caiodassumpcao <caio.rodri.d@gmail.com>
2023-09-23 16:03:41 -0300

    Feat modulo_produt_list.rb: Training module structure, listing products.

---
+++ lib/modulo_produt_list.rb
@@ -0,0 +1,16 @@
+module ListagemProdutos
+  def listar_produtos
+    produtos = %w[P1 P2 P3 P4 P5]
+    produtos.each_with_index do |produto, index|
+      puts "#{index + 1}. #{produto}"
+    end
+  end
+end
+
+class Loja
+  include ListagemProdutos
+end
+
+loja = Loja.new
+loja.listar_produtos
+
