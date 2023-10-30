3819f57cecf69036e76026c838be40d7899fc63e
caiodassumpcao <caio.rodri.d@gmail.com>
2023-10-30 14:04:58 -0300

    Fix expressoes_regulares.rb: Operating with Time

--- lib/expressoes_regulares.rb
+++ lib/expressoes_regulares.rb
@@ -56,18 +56,76 @@ puts /\?/.match('Tudo bem?')
 puts Math.sqrt(16)
 puts Math.cbrt(64)
 puts Math.exp(4)
-puts Math.log10(10)
 puts Math.log(Math::E)
-puts Math
-puts Math
-puts Math
-puts Math
-puts Math
-puts Math
-puts Math
-puts Math
-puts Math
-puts Math
-puts Math
-puts Math
-puts Math
+puts Math.log10(10)
+puts Math.log2(16)
+puts Math.sin(Math::PI / 0)
+puts Math.cos(Math::PI / 1)
+puts Math.tan(Math::PI / 1)
+puts Math.asin(0.5)
+puts Math.acos(0.5)
+puts Math.atan(1)
+puts Math.sinh(1)
+puts Math.cosh(1)
+puts Math.tanh(1)
+puts Math.hypot(3, 4)
+puts Math::PI
+puts Math::E
+
+# TIME = Expressão regular para data e hora
+require 'time'
+require 'tzinfo'
+# Time.now - Horário atual
+# Time.now.year - Ano atual
+# Time.now.month - Mês atual
+# Time.now.day - Dia atual
+# Time.now.wday - Dia da semana atual
+# Time.now + 3600 - Adicionar ou subtrair segundos de uma data e hora
+# time1 < time2 - Comparar duas datas
+# Time.now.strftime("%Y-%m-%d %H:%M:%S") - Formatar data e hora como uma string
+# Time.parse(x) - Converter string em um objeto time
+# time1 - time2 - Calcular diferença em segundos de duas datas
+# Time.now.to_i - Converter um objeto Time em um timestamp Unix (tempo em segundos desde a época)
+# Time.at(timestamp) - Converter objeto Time a partir de uma timestamp Unix
+# Definir uma zona de tempo específica(Fuso horário):
+#     require 'tzinfo'
+#     tz = TZInfo::Timezone.get('American/New_York')
+#     time_ny = Time.now.getlocal(tz)
+#     puts time_ny
+
+current_time = Time.now
+year = Time.now.year
+month = Time.now.month
+day = Time.now.day
+wday = Time.now.wday
+current_future_time = Time.now + 3600
+compare_times = current_time < current_future_time
+formated_time = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+time_conv1 = "2023-10-31 15:30:00"
+convert_time = Time.parse(time_conv1)
+calc_time = current_time - current_future_time
+timestamp = Time.now.to_i
+timestamp2 = 1635620400
+convert_time2 = Time.at(timestamp2)
+begin
+  tz = TZInfo::Timezone.get('America/New_York')  # Corrija o identificador do fuso horário.
+  time_ny = Time.now.getlocal(tz)
+  puts "Fuso horário New York: #{time_ny}"
+rescue TZInfo::InvalidTimezoneIdentifier => e
+  puts "Erro: #{e.message}"
+end
+
+
+puts "Horario atual:#{current_time}"
+puts "Ano atual:#{year}"
+puts "Mês atual:#{month}"
+puts "Dia atual:#{day}"
+puts "Dia da semana atual:#{wday}"
+puts "Adicionando 1 hora a data atual:#{current_future_time}"
+puts "Comparando datas:#{compare_times}"
+puts "Formatando data:#{formated_time}"
+puts "Converter string em objeto time:#{convert_time}"
+puts "Calculando diferença entre times:#{calc_time}"
+puts "Converter um objeto Time em um timestamp Unix: #{timestamp}"
+puts "Converter objeto Time a partir de uma timestamp Unix:#{convert_time2}"
+puts "Fusorario New York#{time_ny}"
