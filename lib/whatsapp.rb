33681b0b1d432b1e7c65e5c8e0dd9be62a1a7107
caiodassumpcao <caio.rodri.d@gmail.com>
2023-10-31 12:03:21 -0300

    Feat whatsapp.rb: Formating whastapp number

--- lib/whatsapp.rb
+++ lib/whatsapp.rb
@@ -16,3 +16,20 @@ verificacao_wpp("(11) 9 9999-9999")
 verificacao_wpp("(99) 1 2345-6789")
 verificacao_wpp("11999999999")
 verificacao_wpp("(22) 3 45678901")
+
+def formatar_numero(numero)
+
+  # Remove qualqier caracter não numérico
+  numero = numero.gsub(/\D/, '')
+
+  if numero.length == 11
+    numero_formatado = "(#{numero[0..1]}) #{numero[2]} #{numero[3..6]}-#{numero[7..10]}"
+    puts "Número formatado: #{numero_formatado}"
+  else
+    puts "Não é possivel formatar este número, digite o numero novamente"
+  end
+end
+
+formatar_numero("11987654321")
+formatar_numero("99123456789")
+formatar_numero("1234567890")
