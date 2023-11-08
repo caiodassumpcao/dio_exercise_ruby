814c401c1dc6a241ca11fb64ddce8f5918a7a5ee
caiodassumpcao <caio.rodri.d@gmail.com>
2023-11-08 12:35:00 -0300

    Fix desafio_web_nokogiri.rb: Web comunication, Print links, title and extract tables

--- desafio_web_nokogiri.rb
+++ desafio_web_nokogiri.rb
@@ -1,2 +1,27 @@
+#aparecer o conteudo de 'P' em vez de 'H1'
+
 require 'nokogiri'
 require 'net/http'
+
+https = Net::HTTP.new('example.com', 443)
+https.use_ssl = true
+response = https.get('/')
+
+if response.code == '200'
+  doc = Nokogiri::HTML(response.body)
+
+  paragraphs = doc.css('p')
+  n = 1
+  puts "Parágrafos encontrados:"
+  if paragraphs.any?
+
+    paragraphs.each do |p|
+      puts "#{n}° parágrafo: #{p.content}"
+      n += 1
+      puts ""
+    end
+  else
+    puts "Nenhum parágrafo encontrado"
+  end
+end
+
