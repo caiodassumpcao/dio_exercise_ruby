5802f5743621eb6f2bcba6d6217868ece40353d1
caiodassumpcao <caio.rodri.d@gmail.com>
2023-11-08 11:06:08 -0300

    Fix webscrap.rb.rb:Web comunication, Print links, title and extract tables

--- lib/webscrap.rb
+++ lib/webscrap.rb
@@ -1,18 +1,42 @@
 require 'nokogiri'
-
 require 'net/http'
 
-https = Net::HTTP.new('exemple.com', 433)
-
+# Realizando umma única solicitação HTTPS
+https = Net::HTTP.new('example.com', 443)
 https.use_ssl = true
+response = https.get('/')
 
-response = https.get.("/")
+# Verificando se a solicitação foi bem sucedida
+if response.code == '200'
+  doc = Nokogiri::HTML(response.body)
 
-doc = Nokogiri::HTML(response.body)
+  # Printando Titulo
+  h1 = doc.at('h1')
+  if h1
+    puts "Título: #{h1.content}"
+  else
+    puts "Elemento h1 não encontrado na página."
+  end
 
-h1 = doc.at('h1')
-puts h1.content
+  # Printando links
+  links = doc.css('a')
 
-def teste(name)
-  if
+  puts "Links encontrados:"
+  links.each do |link|
+    puts link['href']
+  end
+
+
+  # Extrair info de tabelas
+  table = doc.css('table')
+
+  table.css('tr').each do |row|
+    cells = row.css('td')
+
+    puts cells[0].text.strip if cells[0]
+  end
 end
+
+
+
+
